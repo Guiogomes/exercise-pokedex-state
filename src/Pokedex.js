558e0ddb94fc42e9864ffedd8e7b7d63fc8c1706
Guilherme Gomes <ggs.arquiteto@gmail.com>
2021-07-25 19:17:12 -0300

    resolve o filtro de pokemons, mas precisa lidar com o botao next pokemon

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -11,6 +11,11 @@ class Pokedex extends React.Component {
         };
         this.handleNextButtonClick = this.handleNextButtonClick.bind(this);
         this.handleFireButtonClick = this.handleFireButtonClick.bind(this);
+        this.handlePsychicButtonClick = this.handlePsychicButtonClick.bind(this);
+        this.handlePoisonButtonClick = this.handlePoisonButtonClick.bind(this)
+        this.handleElectricButtonClick = this.handleElectricButtonClick.bind(this)
+        this.handleDragonButtonClick = this.handleDragonButtonClick.bind(this)
+        this.handleNormalButtonClick = this.handleNormalButtonClick.bind(this)
         this.filteredPokemon = this.filteredPokemon.bind(this);
     }
     
@@ -18,24 +23,61 @@ class Pokedex extends React.Component {
         const { currentPokemon, pokemonType } = this.state;
         if(pokemonType !== 'all') {
             const filteredPokemon = pokemons.filter((pokemon) => pokemon.type === pokemonType)
-            return <Pokemon pokemon={filteredPokemon[currentPokemon]}/>            
+            return <Pokemon pokemon={filteredPokemon[currentPokemon]}/>
         }
-        return <Pokemon pokemon ={pokemons[currentPokemon]}/>;   
+        return <Pokemon pokemon ={pokemons[currentPokemon]}/>;
     }
     
     handleNextButtonClick() {
         this.setState(({ currentPokemon }) => (
             {currentPokemon: (currentPokemon + 1) % pokemons.length,}
-            ));
-        }
-        
-    handleFireButtonClick() {
-        this.setState(({ pokemonType }) => ({
-                currentPokemon: 0,
-                pokemonType: pokemonType.replace(pokemonType, 'fire')
-            }))
+        ));
     }
-        
+
+    
+
+    handleFireButtonClick() {
+        this.setState(() => ({
+                currentPokemon: 0,
+                pokemonType: 'Fire'
+        }));
+    }
+    
+    handlePsychicButtonClick() {
+        this.setState(() => ({
+                currentPokemon: 0,
+                pokemonType: 'Psychic'
+        }))
+    }
+
+    handlePoisonButtonClick() {
+        this.setState(() => ({
+                currentPokemon: 0,
+                pokemonType: 'Poison'
+        }))
+    }
+
+    handleElectricButtonClick() {
+        this.setState(() => ({
+                currentPokemon: 0,
+                pokemonType: 'Electric'
+        }))
+    }
+
+    handleDragonButtonClick() {
+        this.setState(() => ({
+                currentPokemon: 0,
+                pokemonType: 'Dragon'
+        }))
+    }
+
+    handleNormalButtonClick() {
+        this.setState(() => ({
+                currentPokemon: 0,
+                pokemonType: 'Normal'
+        }))
+    }
+
     render() {
         const { pokemons } = this.props;
         return (
@@ -43,6 +85,12 @@ class Pokedex extends React.Component {
                 {this.filteredPokemon(pokemons)}
                 <button onClick={this.handleNextButtonClick} className="btns next-pokemon">Next pokemon</button>
                 <button onClick={this.handleFireButtonClick} className="btns fire">Fire type</button>
+                <button onClick={this.handlePsychicButtonClick} className="btns psychic">Psychic type</button>
+            	<button onClick={this.handlePoisonButtonClick} className="btns poison">Poison type</button>
+                <button onClick={this.handleElectricButtonClick} className="btns eletric">Eletric type</button>
+				<button onClick={this.handleDragonButtonClick} className="btns dragon">Dragon type</button>
+				<button onClick={this.handleNormalButtonClick} className="btns normal">Normal type</button>	
+
             </div>
         );
     }
