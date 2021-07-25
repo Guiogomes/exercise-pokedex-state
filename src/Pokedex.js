e2a2f8c88d4c1c8c62759ce1e7268e4bd3bda8e8
Guilherme Gomes <ggs.arquiteto@gmail.com>
2021-07-25 17:42:31 -0300

    tenta resolver o problema de filtragem de tipo

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -7,31 +7,40 @@ class Pokedex extends React.Component {
         super()
         this.state = {
             currentPokemon: 0,
-            firePokemon: 'fire',
-            
-        }
-        this.handleNextButtonClick = this.handleNextButtonClick.bind(this)
-        this.handleFireButtonClick = this.handleFireButtonClick.bind(this)
+            pokemonType: 'all',           
+        };
+        this.handleNextButtonClick = this.handleNextButtonClick.bind(this);
+        this.handleFireButtonClick = this.handleFireButtonClick.bind(this);
+        this.filteredPokemon = this.filteredPokemon.bind(this);
     }
-
+    
+    filteredPokemon(pokemons) {
+        const { currentPokemon, pokemonType } = this.state;
+        if(pokemonType !== 'all') {
+            const filteredPokemon = pokemons.filter((pokemon) => pokemon.type === pokemonType)
+            return <Pokemon pokemon={filteredPokemon[currentPokemon]}/>            
+        }
+        return <Pokemon pokemon ={pokemons[currentPokemon]}/>;   
+    }
+    
     handleNextButtonClick() {
         this.setState(({ currentPokemon }) => (
             {currentPokemon: (currentPokemon + 1) % pokemons.length,}
-        ));
-    }
-
+            ));
+        }
+        
     handleFireButtonClick() {
-        this.setState(({ firePokemon }) => {
-            const filterFirePokemons = pokemons.filter(pokemon => pokemon.type === firePokemon);
-            return <Pokemon pokemon={filterFirePokemons[this.state.currentPokemon]} />
-        });
+        this.setState(({ pokemonType }) => ({
+                currentPokemon: 0,
+                pokemonType: pokemonType.replace(pokemonType, 'fire')
+            }))
     }
-
+        
     render() {
         const { pokemons } = this.props;
         return (
             <div className="pokedex">
-                <Pokemon pokemon={pokemons[this.state.currentPokemon]} />
+                {this.filteredPokemon(pokemons)}
                 <button onClick={this.handleNextButtonClick} className="btns next-pokemon">Next pokemon</button>
                 <button onClick={this.handleFireButtonClick} className="btns fire">Fire type</button>
             </div>
